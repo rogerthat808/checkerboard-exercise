6de900dc9a7955dfc00db63a57f828950d19f724
Rogelio <roger.cruz808@gmail.com>
2023-11-15 13:26:30 -0800

    Checkboard with random colors

--- script.js
+++ script.js
@@ -16,42 +16,20 @@ container.style.width = "800px"
 container.style.backgroundColor = "blue"
 container.style.display = "flex"
 container.style.flexWrap = "wrap"
+document.body.appendChild(container)
 
-document.body.appendChild(container) // container inside the body
+// function that creates a random colors
+
 
 for (let i = 0; i < 64; i++) {
     let tile = document.createElement('div')
     tile.style.width = "12.5%"
     tile.style.height = "12.5%"
     if (Math.floor((i / 8) + i) % 2 === 0) {
-        tile.style.backgroundColor = "red"
+        tile.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
         container.appendChild(tile)
     } else {
-        tile.style.backgroundColor = "black"
+        tile.style.backgroundColor = "#" + (Math.random()*0xFFFFFF<<0).toString(16);
         container.appendChild(tile)
     }
 }
-
-
-// for (let i = 1; i <= 8; i++) {
-//     let row = document.createElement('div')
-//     row.style.width = "12.5%"
-//     row.style.height = "12.5%"
-//     for (let e = 1; e <= 8; e++) {
-//         let column = document.createElement('div')
-
-//         if (i % 2 === 0) {
-//             if (e % 2 === 0) {
-//                 row.style.backgroundColor = "black"
-//             }
-//         } 
-//         if (i % 2 !== 0) {
-//             if (e % 2 !== 0 ) {
-//                 row.style.backgroundColor = "red"
-//             }
-//         }
-
-//         row.append(column)
-//     }
-//     container.append(row)
-// }
