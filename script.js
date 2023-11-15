d1cce9dc0b334eb699b18053a80d1590299ff9b1
Rogelio <roger.cruz808@gmail.com>
2023-11-15 15:09:44 -0800

    gradient added

--- script.js
+++ script.js
@@ -24,34 +24,12 @@ for (let i = 0; i < 64; i++) {
     tile.style.width = "12.5%"
     tile.style.height = "12.5%"
     if (Math.floor((i / 8) + i) % 2 === 0) {
-        tile.style.backgroundColor = "red"
+        let lightness = 100 - (i * 100 / 64);
+        tile.style.backgroundColor = `hsl(0, 80%, ${lightness}%)`;
         container.appendChild(tile)
     } else {
-        tile.style.backgroundColor = "black"
+        let blueness = 100 - (i * 100 / 64);
+        tile.style.backgroundColor = `hsl(240, 50%, ${blueness}%)`
         container.appendChild(tile)
     }
 }
-
-
-// for (let i = 1; i <= 8; i++) {
-//     let row = document.createElement('div')
-//     row.style.width = "12.5%"
-//     row.style.height = "12.5%"
-//     for (let e = 1; e <= 8; e++) {
-//         let column = document.createElement('div')
-
-//         if (i % 2 === 0) {
-//             if (e % 2 === 0) {
-//                 row.style.backgroundColor = "black"
-//             }
-//         } 
-//         if (i % 2 !== 0) {
-//             if (e % 2 !== 0 ) {
-//                 row.style.backgroundColor = "red"
-//             }
-//         }
-
-//         row.append(column)
-//     }
-//     container.append(row)
-// }
