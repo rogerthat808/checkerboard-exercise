5f6935ab7719c17937f3b4ff2956da85c7b7d773
Rogelio <roger.cruz808@gmail.com>
2023-11-15 15:56:21 -0800

    Flashing

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// Hints
-
-// Create a container for your tiles with the following styles:
-// display = "flex"
-// flexWrap = "wrap"
-// width = "800px"
-// height = "800px"
-// Each tile should have the following styles:
-// width = "12.5%"
-// height = "12.5%"
-
 
 let container = document.createElement('div')
 container.style.height = "800px"
@@ -19,17 +8,22 @@ container.style.flexWrap = "wrap"
 
 document.body.appendChild(container) // container inside the body
 
-for (let i = 0; i < 64; i++) {
-    let tile = document.createElement('div')
-    tile.style.width = "12.5%"
-    tile.style.height = "12.5%"
-    if (Math.floor((i / 8) + i) % 2 === 0) {
-        let lightness = 100 - (i * 100 / 64);
-        tile.style.backgroundColor = `hsl(0, 80%, ${lightness}%)`;
-        container.appendChild(tile)
-    } else {
-        let blueness = 100 - (i * 100 / 64);
-        tile.style.backgroundColor = `hsl(240, 50%, ${blueness}%)`
-        container.appendChild(tile)
+setInterval(() => {
+    container.innerHTML = ''
+    for (let i = 0; i < 64; i++) {
+        let tile = document.createElement('div')
+        tile.style.width = "12.5%"
+        tile.style.height = "12.5%"
+        if (Math.floor((i / 8) + i) % 2 === 0) {
+            //let lightness = 100 - (i * 100 / 64);
+            //tile.style.backgroundColor = `hsl(0, 80%, ${lightness}%)`;
+            tile.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+            container.appendChild(tile)
+        } else {
+            //let blueness = 100 - (i * 100 / 64);
+            //tile.style.backgroundColor = `hsl(240, 50%, ${blueness}%)`
+            tile.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+            container.appendChild(tile)
+        }
     }
-}
+}, 2000);
